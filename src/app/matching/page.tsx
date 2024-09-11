8ff53521e59922e0bf09901b4cce5704ee88ff8d
kangy106 <kangy106@gmail.com>
2024-09-11 21:36:00 +0900

    マッチングページの追加

---
+++ src/app/matching/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+import Loading from "../../components/Loading";
+import React from 'react';
+import { useEffect, useState } from 'react';
+
+type MatchingData = {
+    name: string[];
+    zoomLink: string;
+    thema: string;
+};
+
+const matchingData: MatchingData[] = [
+    {
+        name: ['かずき', 'せりな', 'たくま', 'じゅん', 'ゆうすけ'],
+        zoomLink: 'https://zoom.us/j/1234567890',
+        thema: '幸せの定義とは？',
+    },
+];
+
+
+export default function Page() {
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    useEffect(() => {
+        setTimeout(() => {
+            setIsLoading(false);
+        }, 3000);
+    }, []);
+    return (
+        <div>
+            {isLoading && <Loading />}
+            {!isLoading && (
+                <div>
+                    <h1 className="text-3xl font-bold">マッチングしました！</h1>
+                    <div className="mt-8">
+                        {matchingData.map((data, index) => (
+                            <div key={index} className="border p-4 mb-4">
+                                <h2 className="text-xl font-bold">お題: {data.thema}</h2>
+                                <div className="mt-4">
+                                    <h3 className="text-lg font-bold">メンバー</h3>
+                                    <ul className="list-disc list-inside mt-2">
+                                        {data.name.map((name, index) => (
+                                            <li key={index}>{name}</li>
+                                        ))}
+                                    </ul>
+                                </div>
+                                <div className="mt-4">
+                                    <a href={data.zoomLink} className="text-blue-500 underline">Zoomリンク</a>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
+                </div>
+            )}
+        </div>
+
+    );
+}
---
+++ src/components/Loading.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Loading() {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <div className="animate-ping h-5 w-5 bg-blue-700 rounded-full"></div>
+        <div className="animate-ping h-5 w-5 bg-pink-700 rounded-full mx-10"></div>
+        <div className="animate-ping h-5 w-5 bg-blue-700 rounded-full"></div>
+      </div>
+    );
+}
